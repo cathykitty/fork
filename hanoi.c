161da757fefb1d8e5547557e74f5cf9ac9ebfaf6
Kimsunyoung <cathykitty@naver.com>
2014-05-16 14:25:27 +0900

    sun commit

---
+++ hanoi.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/types.h>
+#include<stdlib.h>
+
+#define TOTALFORK 10
+void movehanoi(int num,char from,char temp,char to);
+int main(void){
+	int num;
+	pid_t pids;
+	int runProcess = 0;
+	printf("How many floors?\n");
+	scanf("%d",&num);
+	while(runProcess<TOTALFORK){
+		pids = fork();
+		if(pids<0){
+			return -1;
+		}else if(pids==0){
+			printf("Child ID:%d\n",getpid());
+			printf("Child\n");
+			movehanoi(num,'A','B','C');
+			return 0;
+		}else{
+			printf("Parents ID:%d\n",getpid());
+			printf("Parents\n");
+		}
+		runProcess++;
+	}
+	return 0;
+}
+void outputmove(int num,char from,char to){
+printf("%d번째 원판이 %c에서 %c로 이동\n",num,from,to);
+}
+
+void movehanoi(int num,char from,char temp,char to){
+	if(num==1){
+		outputmove(num,from,to);
+	}
+	else{
+		movehanoi(num-1,from,to,temp);
+		outputmove(num,from,to);
+		movehanoi(num-1,temp,from,to);
+}
+}
